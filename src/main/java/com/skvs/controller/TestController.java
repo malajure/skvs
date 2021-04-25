7cddbb07866f652f41694979a8c09cbde3a888b6
malajure.santosh <malajure.santosh@gmail.com>
2021-04-25 19:42:45 +0530

    added second api

--- src/main/java/com/skvs/controller/TestController.java
+++ src/main/java/com/skvs/controller/TestController.java
@@ -1,6 +1,14 @@
 package com.skvs.controller;
 
-class TestController{
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 
+class TestController {
+
+	@GetMapping("/test2")
+	public String test2() {
+		return "test2";
+	}
 }
